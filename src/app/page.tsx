3b6f31d18c94e9a3ff851d6b8aef494f802b7b9f
Sozai83 <chikushi3.227@gmail.com>
2023-09-23 13:51:53 +1000

    added first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client"
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
+import { useState } from 'react'
 
 export default function Home() {
+  const [openDex, setOpenDex] = useState(false);
+  const clickOpenDex = () => setOpenDex(true);
   return (
     <main className={styles.main}>
       <section className={styles.description}>
+        <Image
+          src="/pokedex_top.jpg"
+          width={400}
+          height={700}
+          alt="pokedex top"
+          className={styles.pokedexTop}
+        />
+        <button onClick={clickOpenDex}>
+          Open Pokedex
+        </button>
       </section>
     </main>
   )
